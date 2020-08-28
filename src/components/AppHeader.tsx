5f7cd8d9d357f32d96fcf4914e3a5001cfee3d6d
Joseph Alvizo <alvizoj@uci.edu>
2020-08-27 19:49:45 -0700

    Navbar - made titleText stack for mobile
    Created media query for desktop-size navbar

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,10 +7,6 @@ import { useLocation } from "react-router-dom";
 export default function AppHeader() {
     let location: string = useLocation().pathname;
     //Use location.pathname for active buttons.
-    console.log(location);
-    let isHome: boolean = location == "/home" ? true : false;
-
-    console.log(isHome);
 
     return (
         <header className="Header">
@@ -21,13 +17,13 @@ export default function AppHeader() {
             </h1>
             <Link
                 to="/home"
-                className={location == "/home" ? "activeButton" : ""}
+                className={location === "/home" ? "activeButton" : ""}
                 id="homeButton"
             >
                 Home
             </Link>
             <Link
-                className={location == "/gallery" ? "activeButton" : ""}
+                className={location === "/gallery" ? "activeButton" : ""}
                 to="/gallery"
                 id="galleryButton"
             >
