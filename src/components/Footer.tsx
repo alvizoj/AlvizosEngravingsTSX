722ac17f904fa19a8f62449fbbc417f2da19149d
Joseph Alvizo <alvizoj@uci.edu>
2020-08-28 23:05:27 -0700

    Footer: year automatically updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import "../CSS/Footer.css";
 
 export default function Footer() {
+    let currentYear = new Date().getFullYear();
     return (
         <footer className="Footer">
-            <b>{"\u00A9"} 2019 Joseph Alvizo. All rights reserved.</b>
+            <b>
+                {"\u00A9"} {currentYear} Joseph Alvizo. All rights reserved.
+            </b>
         </footer>
     );
 }
