e382e247161a706b05c1bcbcc7dd8568e866cede
Joseph Alvizo <alvizoj@uci.edu>
2020-08-31 23:39:58 -0700

    NAVBAR 1.0

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,32 +3,57 @@ import "../CSS/AppHeader.css";
 import { Link } from "react-router-dom";
 import StyledLink from "./StyledLink";
 import { useLocation } from "react-router-dom";
+import { stringify } from "querystring";
 
 export default function AppHeader() {
-    let location: string = useLocation().pathname;
+    let path: string = useLocation().pathname;
     //Use location.pathname for active buttons.
 
+    function openHamburgerMenu() {
+        let x: HTMLElement = document.getElementById("myTopnav") as HTMLElement;
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
+    // function setActiveClass(): string {
+    //     let result: string;
+    //     if (path === "/home") {
+    //         result
+    //     }
+    // }
+
     return (
-        <header className="Header">
-            <h1 className="HeaderTitleText">
-                <Link id="titleText" to="/home">
-                    Alvizo's Engravings
+        <header className="topnav" id="myTopnav">
+            <Link id="headerTitleText" to="/home">
+                <h1 className="HeaderTitleText">Alvizo's Engravings</h1>
+            </Link>
+            <div className="Flex">
+                <Link
+                    id="homeButton"
+                    to="/home"
+                    className={path === "/home" ? "activeButton" : ""}
+                >
+                    Home
                 </Link>
-            </h1>
-            <Link
-                to="/home"
-                className={location === "/home" ? "activeButton" : ""}
-                id="homeButton"
+                <Link
+                    id="galleryButton"
+                    to="/gallery"
+                    className={path === "/gallery" ? "activeButton" : ""}
+                >
+                    Gallery
+                </Link>
+            </div>
+
+            <a
+                href="javascript:void(0);"
+                className="icon"
+                onClick={openHamburgerMenu}
             >
-                Home
-            </Link>
-            <Link
-                className={location === "/gallery" ? "activeButton" : ""}
-                to="/gallery"
-                id="galleryButton"
-            >
-                Gallery
-            </Link>
+                <i className="fa fa-bars"></i>
+            </a>
         </header>
     );
 }
