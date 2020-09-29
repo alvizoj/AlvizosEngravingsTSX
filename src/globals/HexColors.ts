3538d66e7c4338ff32298d457cf13cb5dd7efc70
Joseph Alvizo <alvizoj@uci.edu>
2020-09-28 20:30:22 -0700

    Random fixes

--- src/globals/HexColors.ts
+++ src/globals/HexColors.ts
@@ -3,4 +3,4 @@ export const JET = "#2F2F2F";
 export const BROWN = "#654321";
 export const PINK = "#9E6F73";
 export const GRAY = "#919094";
-export const BLACK = "0A070D";
+export const BLACK = "#0A070D";
