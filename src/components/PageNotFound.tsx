7512180eb854d91fb886eff87b14c485c3dfda32
Joseph Alvizo <alvizoj@uci.edu>
2020-09-29 00:17:42 -0700

    Added Error logo svg to PageNotFound component

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -2,10 +2,9 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../CSS/HomeScreen.css";
 import StyledLink from "./StyledLink";
-
-function GiveDropboxImageLink(id: string): string {
-    return "https://www.dropbox.com/s/" + id + "?raw=1";
-}
+import { ReactComponent as AlertIcon } from "../resources/alert-circle-outline.svg";
+import Yee from "../resources/Yee.svg";
+import "../CSS/PageNotFound.css";
 
 export default function PageNotFound() {
     useEffect(() => {
@@ -14,21 +13,22 @@ export default function PageNotFound() {
 
     return (
         <main>
-            <h1 style={{ fontSize: "4vw" }}>
-                Whoops! Pet the doggo to return Home
-            </h1>
+            {/*<h1 style={{ fontSize: "4vw" }}>*/}
+            {/*    Whoops! Pet the doggo to return Home*/}
+            {/*</h1>*/}
+            <img className="ErrorLogo" src={require("../resources/Yee.svg")} />
 
-            <Link to="/home">
-                <img
-                    className="ResponsiveImage"
-                    src={GiveDropboxImageLink(
-                        "5ctzsnl05uxjpmn/20141026_091534.jpg"
-                    )}
-                    alt="pic"
-                />
-            </Link>
+            {/*<Link to="/home">*/}
+            {/*    <img*/}
+            {/*        className="ResponsiveImage"*/}
+            {/*        src={GiveDropboxImageLink(*/}
+            {/*            "5ctzsnl05uxjpmn/20141026_091534.jpg"*/}
+            {/*        )}*/}
+            {/*        alt="pic"*/}
+            {/*    />*/}
+            {/*</Link>*/}
 
-            <h3 style={{ fontSize: "3vw" }}>Sorry, this page doesn't exist.</h3>
+            <h3 className="NotFoundText">Sorry, this page doesn't exist.</h3>
             <br />
             <StyledLink
                 buttonText="Return Home"
@@ -36,9 +36,9 @@ export default function PageNotFound() {
                 paddingBottom="1rem"
                 backgroundColor="blue"
                 textColor="gray"
-                fontSize="10vw"
+                fontSize="3vw"
             />
-            <h2 style={{ fontSize: "2vw" }}>
+            <h2 className="SecondaryText">
                 Alternatively, you can also view my
                 <Link to="/gallery"> GALLERY</Link>
             </h2>
